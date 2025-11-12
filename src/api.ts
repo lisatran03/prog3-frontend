4ce1ee21cd67b0abaf7b6ad8cc946b75582295b1
lisatran03 <lisa.tran@student.htw-berlin.de>
2025-11-13 00:25:26 +0100

    M3 Fix für FE deployen

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
-});
-
 export function getRecipes() {
-  return api.get("/recipes");
+  return axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/recipes`);
 }
 
 export function getCategories() {
-  return api.get("/categories");
+  return axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/categories`);
 }
+
