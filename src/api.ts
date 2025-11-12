6e6a3875681523d80a891e2426ca19a853e51234
lisatran03 <lisa.tran@student.htw-berlin.de>
2025-11-12 23:34:11 +0100

    M3

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+});
+
 export function getRecipes() {
-  return axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/recipes`);
+  return api.get("/recipes");
 }
 
+export function getCategories() {
+  return api.get("/categories");
+}
